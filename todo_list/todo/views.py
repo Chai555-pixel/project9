ccc9e4e7add9f0befeb916b288e58f18a4e90eb4
chai555-pixel <chaithrak636016@gmail.com>
2024-12-09 11:13:23 +0530

    todo

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,6 +1,6 @@
 # todo/views.py
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import Task
 
 # Home view
@@ -12,3 +12,9 @@ class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'  # The context variable for the task list
     template_name = 'todo/task_list.html'  # Template name (optional, defaults to 'todo_list/task_list.html')
+
+# Task detail view
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'  # The context variable name in the template
+    template_name = 'todo/task_detail.html'  # Optional, defaults to 'todo/task_detail.html'
