267f35f0a5fe60b6f7f03e374f9ced48fb02da83
chai555-pixel <chaithrak636016@gmail.com>
2024-12-09 11:42:29 +0530

    initial commit

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,9 +1,10 @@
 # todo/urls.py
 from django.urls import path
-from .views import home, TaskList, TaskDetail
+from .views import home, TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', home, name='home'),  # Home page route
     path('tasks/', TaskList.as_view(), name='tasks'),  # Task list route
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),  # Task detail route
+    path('task/create/', TaskCreate.as_view(), name='task-create'),  # Create task route
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,6 +1,9 @@
 # todo/views.py
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib import messages
+from django.urls import reverse_lazy
 from .models import Task
 
 # Home view
@@ -13,8 +16,21 @@ class TaskList(ListView):
     context_object_name = 'tasks'  # The context variable for the task list
     template_name = 'todo/task_list.html'  # Template name (optional, defaults to 'todo_list/task_list.html')
 
+
 # Task detail view
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'  # The context variable name in the template
     template_name = 'todo/task_detail.html'  # Optional, defaults to 'todo/task_detail.html'
+
+# TaskCreate view to handle the creation of a new task
+class TaskCreate(CreateView):
+    model = Task  # The model for the view
+    fields = ['title', 'description', 'completed']  # Fields to be shown in the form
+    success_url = reverse_lazy('tasks')  # Where to redirect after a successful form submission
+
+    # Customizing the form_valid method to associate the user with the task
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # Set the user to the logged-in user
+        messages.success(self.request, "The task was created successfully.")  # Flash message
+        return super().form_valid(form)  # Proceed with form validation and save
